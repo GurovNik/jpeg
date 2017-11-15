49f392d8ad19237e55dcc0d69396a5b30335a123
Evgeny Sorokin <evgerher1l@gmail.com>
2017-11-15 22:49:15 +0300

    LZW decompress

--- Client/src/Algorithm/LZW.java
+++ Client/src/Algorithm/LZW.java
@@ -1,12 +1,15 @@
 package Algorithm;
 
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LZW {
 
     private int bits;
-    private int size;
+    private int size = 255;
     private final int initialSetAmoint = 8;
     private final int initialDictionarySize = 255;
     private int[] getInput() {
@@ -18,7 +21,7 @@ public class LZW {
         return input[index];
     }
 
-    private boolean isInTable(String ) {
+    private boolean isInTable(String s) {
         return false;
     }
 
@@ -38,8 +41,74 @@ public class LZW {
         return null;
     }
 
-    public int[] decompress() {
-        Map<Integer, String> dictionary = new HashMap<>();
+    public String decompress(ArrayList<Integer> vals) {
+        Map<Integer, String> dictionary = loadDictionary();
+        ArrayList<String> content = new ArrayList<>();
+        int index = 0;
+        int pW;
+        int cW;
+        String wordPW;
+        String wordCW;
+        String P;
+        String C;
+
+
+        cW = vals.get(index);
+        wordCW = dictionary.get(cW);
+        content.add(wordCW);
+
+
+        for (int i = 1; i < vals.size(); i++) {
+            pW = cW;
+            cW = vals.get(++index);
+            if (dictionary.containsKey(cW)) {
+                wordCW = dictionary.get(cW);
+                content.add(wordCW);
+
+                wordPW = dictionary.get(pW);
+
+                P = wordPW;
+                C = wordCW.substring(0, 1);
+                dictionary.put(++size, P + C);
+            } else {
+                wordPW = dictionary.get(pW);
+
+                P = wordPW;
+                C = wordPW.substring(0, 1);
+                dictionary.put(++size, P + C);
+                content.add(P + C);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String s: content)
+            sb.append(s);
+
+        return sb.toString();
+    }
+
+    public File decompress(File link) {
         return null;
     }
+
+    private Map<Integer, String> loadDictionary() {
+        Map<Integer, String> dict = new HashMap<>();
+        for (int i = 0; i < size + 1; i++) {
+            char c = (char) i;
+            dict.put(i, ""+c);
+        }
+        return dict;
+    }
+
+    public static void main(String[] args) {
+        int values[] = new int[]{(int)'a', (int)'a', (int)'b', (int)'b', 257, 257, (int)'b', (int)'c', (int)'a', 263, (int)'c', (int)'c'};
+        ArrayList<Integer> vals = new ArrayList<>();
+        for (int a: values)
+            vals.add(a);
+        
+        LZW lzw = new LZW();
+        String s = lzw.decompress(vals);
+        System.out.println(s);
+    }
+
 }

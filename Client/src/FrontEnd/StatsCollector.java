eadf4e3fc1d46d2559cf39b5f7d0627cd83ee035
Evgeny Sorokin <evgerher1l@gmail.com>
2017-11-19 23:46:58 +0300

    JSON collects stats

--- Client/src/FrontEnd/StatsCollector.java
+++ Client/src/FrontEnd/StatsCollector.java
@@ -2,10 +2,12 @@ package FrontEnd;
 
 import Algorithm.*;
 import Algorithm.JPEG.JpegCompression;
+import org.json.simple.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import static Algorithm.FileProcessor.getFilenameExtension;
 
@@ -19,6 +21,8 @@ public class StatsCollector {
     private static String[] encodeNames = new String[]{"REPETITION3", "REPETITION5", "REEDMULLER", "HAMMING"};
 
     private static HashMap<Object, String> algName = new HashMap<>();
+    private static HashMap<String, JSONObject> statisticsCompression = new HashMap<>();
+    private static HashSet<JSONObject> statisticsFull = new HashSet<>();
 
     private static String foldersPath = "C:/cygwin64/home/evger/DATA";
     private static File folders = new File(foldersPath);
@@ -34,6 +38,9 @@ public class StatsCollector {
         for (int i = 0; i < encodeNames.length; i++)
             algName.put(encodeAlgorithms[i], encodeNames[i]);
 
+        int success = 0;
+        int counter = 0;
+
         for (int i = 0; i < compressionAlgorithms.length - 2; i++) {
             CompressionAlgorithm alg = compressionAlgorithms[i];
             for (File folder: folders.listFiles()) {
@@ -41,10 +48,25 @@ public class StatsCollector {
                     File files[] = subfolder.listFiles();
                     format = getFilenameExtension(files[0]);
                     for (File item : files) {
+                        JSONObject json = new JSONObject();
+                        long time = System.currentTimeMillis();
                         File compressed = alg.compress(item);
+                        time = System.currentTimeMillis() - time;
+
+                        json.put("message", "");
+                        json.put("format", format);
+                        json.put("compression", Integer.toString(i));
+                        json.put("compression_time", Long.toString(time));
+                        json.put("initial_size", Long.toString(item.getTotalSpace()));
+                        json.put("compressed_size", Long.toString(compressed.getTotalSpace()));
                         String newPath = foldersPath + "/../home/evger/Statistics/" + algName.get(alg) + "/" + compressed + item.getName();
                         boolean t = compressed.renameTo(new File(newPath));
-                        System.out.println(t);
+                        if (t) {
+                            success++;
+                            statisticsCompression.put(compressed.getName(), json);
+                        }
+                        counter++;
+                        System.out.println("Done :: " + counter);
                     }
                 }
             }
